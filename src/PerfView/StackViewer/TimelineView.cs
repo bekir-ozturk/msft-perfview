284e60689ccf91bf7611ab21b60b79c2fb9b1405
Bekir Ozturk <bozturk@microsoft.com>
2022-09-22 18:18:22 +0100

    Group cpu samples under a shared frame

--- src/PerfView/StackViewer/TimelineView.cs
+++ src/PerfView/StackViewer/TimelineView.cs
@@ -41,7 +41,6 @@ namespace PerfView
 
         private void RepopulateVisualsData(CallTree callTree, TimelineVisuals visuals)
         {
-            int minimumBlockSizeInFrames = 30;
             Dictionary<int, List<(StackSourceSample sample, StackInfo info)>> stacksPerThread = new Dictionary<int, List<(StackSourceSample, StackInfo)>>();
 
             callTree.StackSource.ForEach((s) =>
@@ -95,42 +94,24 @@ namespace PerfView
                     visuals.VisualsPerThreadId[st.Key] = works = new List<WorkVisual>();
                 }
 
-                int nextVisualToWrite = 0;
-                for (int i = 0; i < st.Value.Count; i++)
+                int firstSampleOfInterest = st.Value.TakeWhile(s => (int)s.sample.SampleIndex <= visuals.StartingFrame).Count();
+                firstSampleOfInterest = Math.Max(0, firstSampleOfInterest - 1);
+
+                int sampleAtTheEndOfInterest = st.Value.Skip(firstSampleOfInterest).TakeWhile(s => (int)s.sample.SampleIndex < visuals.EndingFrame).Count() + firstSampleOfInterest;
+                sampleAtTheEndOfInterest = Math.Min(st.Value.Count, sampleAtTheEndOfInterest + 1);
+
+                if (FindWorkToDisplay(st.Value, firstSampleOfInterest, sampleAtTheEndOfInterest, 0, 40, 150, works))
                 {
-                    StackSourceSample stackSample = st.Value[i].sample;
-
-                    if ((int)stackSample.SampleIndex < visuals.StartingFrame)
+                    works.Add(new WorkVisual()
                     {
-                        continue;
-                    }
-                    if ((int)stackSample.SampleIndex > visuals.EndingFrame)
-                    {
-                        break;
-                    }
-
-                    if (works.Count == nextVisualToWrite)
-                    {
-                        works.Add(new WorkVisual());
-                    }
-
-                    var visual = works[nextVisualToWrite];
-                    visual.StartingFrame = (int)stackSample.SampleIndex;
-                    visual.EndingFrame = visual.StartingFrame + 1;
-                    visual.IsGroupingSmallWork = false;
-                    visual.DisplayColor = GetRandomColor();
-
-                    nextVisualToWrite++;
-                    if (nextVisualToWrite == 1000)
-                        break; // Don't draw more than 1000 for now.
-                }
-
-                while (works.Count > st.Value.Count)
-                {
-                    works.RemoveAt(works.Count - 1);
+                        StartingFrame = (int)st.Value[firstSampleOfInterest].sample.SampleIndex,
+                        EndingFrame = (int)(st.Value[sampleAtTheEndOfInterest - 1].sample.SampleIndex + 1),
+                        DisplayColor = GetRandomColor(),
+                        DisplayName = "Thread " + st.Key,
+                        IsGroupingSmallWork = true
+                    });
                 }
             }
-            // callTree.FindTreeNode(callTree.StackSource.BaseStackSource.ForEach)
         }
 
         /// <summary>
@@ -138,7 +119,7 @@ namespace PerfView
         /// </summary>
         /// <param name="samples">All samples of the thread.</param>
         /// <param name="startIndex">First sample in the range of interest.</param>
-        /// <param name="endIndex">Last samples in the range of interest (inclusive).</param>
+        /// <param name="endIndex">The first sample after the range of interest has ended.</param>
         /// <param name="frameDepth">The depth that we should start from when iterating over the call stack.</param>
         /// <param name="minimumVisualSampleCount">How small can a WorkVisual be? Represented in # of samples.</param>
         /// <param name="maximumVisualSampleCount">How big a WorkVisual can be before we decide to split it into multiple smaller ones.</param>
@@ -153,28 +134,136 @@ namespace PerfView
             int maximumVisualSampleCount,
             List<WorkVisual> visuals)
         {
-            if (startIndex == endIndex)
-            {
-                if (minimumVisualSampleCount > 1)
-                    return false; // We are too small to be drawn on our own.
-
-                // We have zoomed in so much that even 1 frame can be displayed on the screen.
-                visuals.Add(new WorkVisual()
-                {
-                    StartingFrame = startIndex,
-                    EndingFrame = startIndex + 1,
-                    DisplayColor = GetRandomColor(),
-                    // Let's go to the lowest level frame because that's the code that was actually running
-                    DisplayName = samples[startIndex].stack.FrameNames.Last(),
-                    IsGroupingSmallWork = false
-                });
-            }
-
+            int groupedWorkStart = -1;
+            int lastWorkStart = startIndex;
+            StackSourceFrameIndex? trackedFrame = null;
+            string trackedFrameName = "Self"; // If stack is not deep enough, this means time was spend in the function itself.
             for (int i = startIndex; i <= endIndex; i++)
             {
-                // WIP
+                (StackSourceSample sample, StackInfo stack) = (default, default);
+                StackSourceFrameIndex? currentFrame = null;
+                StackSourceSampleIndex currentSampleIndex;
+                bool noMoreSamplesLeft = i == endIndex;
+
+                if (!noMoreSamplesLeft)
+                {
+                    (sample, stack) = samples[i];
+                    currentFrame = stack.Frames.Length > frameDepth
+                        ? (StackSourceFrameIndex?)stack.Frames[frameDepth]
+                        : null;
+                    currentSampleIndex = sample.SampleIndex;
+                }
+                else
+                {
+                    // We are passed the last sample. We don't have any more samples to know the duration of this execution.
+                    // So let's just assume that this thing has executed for only 1 frame.
+                    currentSampleIndex = samples[i - 1].sample.SampleIndex + 1;
+                }
+
+                if (trackedFrame != currentFrame // The frame is changing. Decide whether to visualize the processed samples.
+                    || noMoreSamplesLeft) // Or, there are no more frames left. Visualize all that was processed.
+                {
+                    // The frame has changed. The last chunk we were building is finished now.
+                    int chunkSize = currentSampleIndex - samples[lastWorkStart].sample.SampleIndex;
+                    if (chunkSize == 0)
+                    {
+                        // Evaluation has just started, nothing to visualize here.
+                    }
+                    else if (chunkSize < minimumVisualSampleCount) // this is too small to visualize. Continue accumulating.
+                    {
+                        if (groupedWorkStart == -1)
+                            groupedWorkStart = lastWorkStart;
+                        else
+                            ; // Group had been started earlier. Keep on.
+
+                        if (noMoreSamplesLeft)
+                        {
+                            // looks like there are on more samples. Visualize this last bit.
+                            if (groupedWorkStart == startIndex)
+                            {
+                                // We have traversed the whole samples, but we have found nothing significant to visualize.
+                                // Instead of putting a big "Other" visual, let the parent know
+                                // so that we can use the caller frame name instead.
+                                return false;
+                            }
+
+                            visuals.Add(new WorkVisual()
+                            {
+                                StartingFrame = groupedWorkStart,
+                                EndingFrame = (int)currentSampleIndex,
+                                DisplayColor = Colors.DimGray,
+                                DisplayName = "Other",
+                                IsGroupingSmallWork = true,
+                            });
+                            groupedWorkStart = -1;
+                        }
+                    }
+                    else
+                    {
+                        // Work we accumulated so far is big enough to be displayed.
+                        // Let's first check one thing though: did we end up finding a huge single chunk for this whole range?
+                        // If yes, there was no point in splitting this further if we already reached the bottom of the call stack.
+                        if (lastWorkStart == startIndex // this started at the beginning
+                            && noMoreSamplesLeft // and went all the way to the end
+                            && !trackedFrame.HasValue) // doesn't even have any frames this deep.
+                        {
+                            // We went to far. Let the caller know that we don't have anything valuable here to visualize.
+                            return false;
+                        }
+
+                        // Some earlier work to be grouped has been accumulating as well. Visualise that first.
+                        if (groupedWorkStart != -1)
+                        {
+                            visuals.Add(new WorkVisual()
+                            {
+                                StartingFrame = groupedWorkStart,
+                                EndingFrame = lastWorkStart,
+                                DisplayColor = Colors.DimGray,
+                                DisplayName = "Other",
+                                IsGroupingSmallWork = true,
+                            });
+                            groupedWorkStart = -1;
+                        }
+
+                        bool createdVisuals = false;
+                        if (chunkSize > maximumVisualSampleCount)
+                        {
+                            // This work is too big to visualise alone. Try to split into multiple pieces.
+                            if (FindWorkToDisplay(samples, lastWorkStart, i, frameDepth+1, minimumVisualSampleCount, maximumVisualSampleCount, visuals))
+                            {
+                                // We managed to split this work. Don't try again.
+                                createdVisuals = true;
+                            }
+                        }
+
+                        if (!createdVisuals)
+                        {
+                            // We haven't been able to split this further. We will just put this work as a single visual.
+                            visuals.Add(new WorkVisual()
+                            {
+                                StartingFrame = (int)samples[lastWorkStart].sample.SampleIndex,
+                                EndingFrame = (int)currentSampleIndex,
+                                DisplayColor = GetRandomColor(),
+                                DisplayName = trackedFrameName,
+                                IsGroupingSmallWork = false,
+                            });
+                        }
+
+                        // Frame changed, one work ended the other is starting.
+                        lastWorkStart = i;
+                    }
+
+                    if (!noMoreSamplesLeft)
+                    {
+                        // Prepare for the next iteration.
+                        trackedFrame = currentFrame;
+                        trackedFrameName = stack.FrameNames.Length > frameDepth
+                            ? stack.FrameNames[frameDepth]
+                            : "Self";
+                    }
+                }
             }
-            return false;
+            return true;
         }
 
         private void UpdateSummaryCanvas(TimelineVisuals visuals)
